1d5fe82b56ff9a49ef8a9de3e57cefc14986a561
JethroWang24 <202311220@fit.edu.ph>
2025-05-13 08:48:09 +0800

    feat: add the index.php, page1.php, and style.css

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AD-Task-1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $name = "Jethro Wang";
+    ?>
+    <div class="section">
+        <h1>Hello, <?php echo $name; ?></h1>
+        <p>Please scroll down to view more</p>
+    </div>
+
+    <div class="section">
+        <h1>Basic Information<br>(Conditional Stament and Declarations)</h1>
+        <?php
+        $school = "FEU Institute of Technology";
+        $course = "BS information Technology";
+        $specialization = "Web and Mobile Development";
+        $year = "2nd Year";
+
+        if ($school == "FEU Institute of Technology" && $course == "BS information Technology") {
+            echo "<p>School: $school</p>";
+            echo "<p>Course: $course</p>";
+            echo "<p>Specialization: $specialization</p>";
+            echo "<p>School year: $year</p>";
+        } else {
+            echo "<p>Information not available.</p>";
+        }
+        ?>
+    </div>
+
+    <div class="section">
+        <h1>Meal Schedule<br>(Conditional Stament and Declarations)</h1>
+        <?php
+        $day = date("l");
+        echo "<p>Today is: $day</p>";
+
+        switch($day){
+            case "Monday":
+                $meals = [
+                    "Breakfast: Fried Rice & Egg",
+                    "Lunch: Chicken Adobo",
+                    "Dinner: Tinola"
+                ];
+                break;
+            case "Tuesday":
+                $meals =[
+                    "Breakfast: Tapsilog",
+                    "Lunch: Sinigang na Baboy",
+                    "Dinner: Grilled Bangus"
+                ];
+                break;
+            case "Wednesday":
+                $meals =[
+                    "Breakfast: Champorado",
+                    "Lunch: Kare-Kare",
+                    "Dinner: Pork BBQ"
+                ];
+                break;
+            case "Thursday":
+                $meals =[
+                    "Breakfast: Pancakes with bacon",
+                    "Lunch: Bistek ",
+                    "Dinner: Ginisang Munggo"
+                ];
+                break;
+            case "Friday":
+                $meals =[
+                    "Breakfast: bread and Milo ",
+                    "Lunch: Fried Tilapia ",
+                    "Dinner: Spaghetti"
+                ];
+            case "Saturday":
+                $meals =[
+                    "Breakfast: Turon ",
+                    "Lunch: Fried Chicken",
+                    "Dinner: Sinigang"
+                ];
+                break;
+            default:
+            $meals =["No meals planned"];
+                
+        }
+
+        foreach ($meals as $meal){
+            echo "<div class=\"box\">$meal</div>";
+        }
+        
+
+        ?>
+        <a href="page2.php" class="btn">Go to the 2nd page</a>
+    </div>
+</body>
+</html>
---
+++ page/page1/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Looping Structures</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="page">
+        <h1>The 7 Days(Looping Incramental)</h1>
+        <div class="scroll-box">
+            <?php
+            $weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            $n = 1;
+            foreach ($weeks as $day){
+                echo "<div class='box'>Day $n: $day</div>";
+                $n++;
+            }
+            ?>
+            </div>
+
+        <h1>Workout Days(Looping Incramental)</h1>
+        <div class="scroll-box">
+            <?php
+            $workouts = ["Pushday", "Pullday", "legday", "Rest day", "Endurance Day", "light workout", "Rest day"];
+            for ($i = 0; $i < count($workouts); $i++) {
+            $n = $i + 1;
+            echo "<div class='box'>Workout $n: {$workouts[$i]}</div>";
+            }
+            ?>
+        </div>
+
+        <h1>Top 5 Recommendation(Looping Decramental)</h1>
+        <div class="scroll-box">
+            <?php
+            $movies =["Hunger by Sitisiri Mongkolsiri","The PURSUIT of Happiness","The Founder (2016)","The Social Network","Breaking bad" ];
+            $count = 5;
+            while ($count > 0){
+                $movieIndex = 5 - $count;
+                $movieName = isset($movies[$movieIndex]) ? $movies[$movieIndex] : '';
+                echo "<div class='box'>Top $count NETFLIX Recommendation : $movieName</div>";
+                $count--;
+            }
+            ?>
+        </div>
+
+        <a href="Index.php" class="btn"> Back to Main</a>
+
+    </div>
+
+
+    
+</body>
+</html>
